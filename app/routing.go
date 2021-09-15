1541dcd42800bbd1c9d006d85f617f1bcadd92f4
parkh130 <hyeonkyeong.park@kddi-web.com>
2021-09-16 03:42:45 +0900

    etc : コンテナNW追加

--- app/routing.go
+++ app/routing.go
@@ -9,7 +9,7 @@ import (
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	r.LoadHTMLGlob("../templates/*")
+	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(c *gin.Context) {
 		c.Data(http.StatusOK,
 			"text/html; charset=utf-8",
--- auth/auth.go
+++ auth/auth.go
@@ -2,28 +2,46 @@ package auth
 
 import (
 	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Jwt struct {
-	AccessToken string
+	AccessToken  string
 	RefreshToken string
 }
 
 type User struct {
-	Email string
+	gorm.Model
+	Email    string
 	Password string
 }
 
 func GetTokenHandler(c *gin.Context) {
-	c.Data(http.StatusOK, 
-		"text/html; charset=utf-8", 
-		[]byte("token"))
+	dsn := "host=192.168.32.2 user=" + os.Getenv("POSTGRES_USER") +
+		" password=" + os.Getenv("POSTGRES_PASSWORD") +
+		" dbname=" + os.Getenv("POSTGRES_DB") +
+		" port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	output := "success"
+
+	if err != nil {
+		output = "fail"
+	}
+
+	db.Create(&User{Email: "t@test.com", Password: "t"})
+
+	c.Data(http.StatusOK,
+		"text/html; charset=utf-8",
+		[]byte(output))
 }
 
 func RenderLoginView(c *gin.Context) {
-	c.HTML(http.StatusOK, 
-		"login.html", 
+	c.HTML(http.StatusOK,
+		"login.html",
 		gin.H{})
 }
 
